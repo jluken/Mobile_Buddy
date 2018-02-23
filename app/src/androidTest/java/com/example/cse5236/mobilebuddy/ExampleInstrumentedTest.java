84d69cc60b2a0ca4c70dd87b5ed9f39889ba33ec
Krishna Ganesan <krishnaganesan@Krishnas-MacBook-Pro.local>
2018-02-22 21:58:55 -0500

    added home screen

--- app/src/androidTest/java/com/example/cse5236/mobilebuddy/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/cse5236/mobilebuddy/ExampleInstrumentedTest.java
@@ -25,3 +25,4 @@ public class ExampleInstrumentedTest {
     }
 }
 // Test comment
+//Test2
---
+++ app/src/main/java/com/example/cse5236/mobilebuddy/HomeScreenActivity.java
@@ -0,0 +1,18 @@
+package com.example.cse5236.mobilebuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * Created by krishnaganesan on 2/22/18.
+ */
+
+public class HomeScreenActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen_activity);
+    }
+}
