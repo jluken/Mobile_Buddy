1249dccd3f0d6f5fd048983a0c9eb2db6a51fe09
Krishna Ganesan <krishnaganesan@Krishnas-MacBook-Pro.local>
2018-03-25 00:05:37 -0400

    first step for pedometer stuff

--- app/src/main/java/com/example/cse5236/mobilebuddy/GoForWalk.java
+++ app/src/main/java/com/example/cse5236/mobilebuddy/GoForWalk.java
@@ -1,13 +1,31 @@
 package com.example.cse5236.mobilebuddy;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class GoForWalk extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class GoForWalk extends AppCompatActivity implements SensorEventListener, StepListener {
+
+    private TextView textView;
+    private StepDetector simpleStepDetector;
+    private SensorManager sensorManager;
+    private Sensor accel;
+    private static final String TEXT_NUM_STEPS = "Number of Steps: ";
+    private int numSteps;
+    private TextView TvSteps;
+    private Button BtnStart;
+    private Button BtnStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +34,65 @@ public class GoForWalk extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        simpleStepDetector = new StepDetector();
+        simpleStepDetector.registerListener(this);
+
+
+        TvSteps = (TextView) findViewById(R.id.tv_steps);
+        BtnStart = (Button) findViewById(R.id.btn_start);
+        BtnStop = (Button) findViewById(R.id.btn_stop);
+
+
+
+        BtnStart.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View arg0) {
+
+                numSteps = 0;
+                sensorManager.registerListener(GoForWalk.this, accel, SensorManager.SENSOR_DELAY_FASTEST);
+
+            }
+        });
+
+
+        BtnStop.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                sensorManager.unregisterListener(GoForWalk.this);
+
             }
         });
     }
+
+
+
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            simpleStepDetector.updateAccel(
+                    event.timestamp, event.values[0], event.values[1], event.values[2]);
+        }
+    }
+
+    @Override
+    public void step(long timeNs) {
+        numSteps++;
+        TvSteps.setText(TEXT_NUM_STEPS + numSteps);
+    }
+
 
 }
---
+++ app/src/main/java/com/example/cse5236/mobilebuddy/StepListener.java
@@ -0,0 +1,11 @@
+package com.example.cse5236.mobilebuddy;
+
+/**
+ * Created by krishnaganesan on 3/21/18.
+ */
+
+public interface StepListener {
+
+    public void step(long timeNs);
+
+}
